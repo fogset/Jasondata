ef17df4a8e22ca94f6a19992cdc45d2b9da2ecd9
Tianhao <zhangth1990@hotmail.com>
2018-03-26 21:18:33 -0600

    finished

--- app/src/main/java/com/example/tianhao/jasondata/MainActivity.java
+++ app/src/main/java/com/example/tianhao/jasondata/MainActivity.java
@@ -5,6 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -30,21 +33,32 @@ public class MainActivity extends AppCompatActivity {
                     data = reader.read();
                 }
 
-
-
-
+                return result;
             }catch (Exception e){
                 e.printStackTrace();
                 return null;
             }
-            return null;
 
         }
 
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
-            Log.i("JSON", s);
+            //Log.i("JSON", s);
+            try {
+                JSONObject jsonObject = new JSONObject(s);
+                String weatherInfo = jsonObject.getString("weather");
+                Log.i("Weather content", weatherInfo);
+                JSONArray arr = new JSONArray(weatherInfo);
+                for( int i=0; i < arr.length(); i++){
+                    JSONObject jsonPart = arr.getJSONObject(i);
+                    Log.i("main", jsonPart.getString("main"));
+                    Log.i("description", jsonPart.getString("description"));
+                }
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+
         }
     }
     @Override
